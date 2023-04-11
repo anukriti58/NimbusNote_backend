19a95b319afbc4ce496de5812125cef54815e95c
Anukriti <anukritishri@gmail.com>
2023-04-11 19:14:24 +0530

    User and Note Schema created

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const mongoURI = "mongodb://0.0.0.0:27017/"
+
+const connectToMongo = ()=> {
+    mongoose.connect(mongoURI)
+        .then(() => console.log('connected to mongo'))
+        .catch((err) => console.log(err));
+}
+
+module.exports = connectToMongo;
---
+++ models/Notes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const NotesSchema = new Schema({
+    title:{
+        type: String,
+        required: true 
+    },
+    description:{
+        type: String,
+        required: true ,
+    },
+    tag:{
+        type: String,
+        default: "General"
+    },
+    date:{
+        type: String,
+        default: date.now
+    },
+  });
+
+  module.exports = mongoose.model('user', NotesSchema);
